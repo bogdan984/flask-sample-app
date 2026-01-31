2cdbcc11f83b31cae16df5f0532672b3493655f7
bogdan <57296038+bogdan984@users.noreply.github.com>
2026-01-31 22:22:02 +0500

    Configure Flask to run on all IPs and port 5000
    Updated app.run() to specify host and port for Flask.

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 # run.py
-
 from app import app
 
 if __name__ == '__main__':
-    app.run()
-
+    # host='0.0.0.0' заставляет Flask слушать все публичные IP адреса ноды
+    # port=5000 — стандартный порт, который мы укажем в настройках Flux
+    app.run(host='0.0.0.0', port=5000)
